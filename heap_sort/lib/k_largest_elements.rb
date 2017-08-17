5456b55f6226ae7dc7c09bb0fc39b08bf3c8909b
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-17 13:58:29 -0700

    fixed k largest elements

--- heap_sort/lib/k_largest_elements.rb
+++ heap_sort/lib/k_largest_elements.rb
@@ -1,14 +1,10 @@
 require_relative 'heap'
 
 def k_largest_elements(arr, k)
-
-  prc = Proc.new { |i, j| i <=> j }
-  (2..arr.size).each do |sub_len|
-    BinaryMinHeap.heapify_up(arr, sub_len - 1, sub_len, &prc)
-  end
-  arr.size.downto(2).each do |sub_len|
-    arr[sub_len - 1], arr[0] = arr[0], arr[sub_len - 1]
-    BinaryMinHeap.heapify_down(arr, 0, sub_len - 1, &prc)
-  end
-  arr[0...k]
+  prc = Proc.new { |i, j| j <=> i }
+  new_arr = []
+  heap = BinaryMinHeap.new(&prc)
+  arr.each { |i| heap.push(i) }
+  k.times { new_arr << heap.extract }
+  new_arr
 end
